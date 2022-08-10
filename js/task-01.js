eaca571a78e8f80dc46920f5668aa0da0da68b96
Roman <romanuch06.88@gmail.com>
2022-08-10 16:55:40 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,15 @@
-// 1.Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// 1.Посчитает и выведет в консоль количество категорий в ul#categories,
+// то есть элементов li.item.
 
-// 2.Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка
-// элемента(тега < h2 >) и количество элементов в категории(всех вложенных в него < li >).
+const listOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", listOfCategories.length);
+
+// 2.Для каждого элемента li.item в списке ul#categories, найдет и выведет
+// в консоль текст заголовка элемента(тега < h2 >) и количество элементов
+// в категории(всех вложенных в него < li >).
+for (let elem of listOfCategories) {
+  let category = elem.firstElementChild.textContent;
+  let quantityElem = elem.lastElementChild.children.length;
+  console.log("Category: ", category);
+  console.log("Elements:", quantityElem);
+}
