fa099cd4a42a6df44f45c59ea859878ec1bfe0bd
Roman <romanuch06.88@gmail.com>
2022-08-10 19:06:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("ul#ingredients");
+
+const elements = ingredients.map((item) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = item;
+  itemEl.classList.add("item");
+  return itemEl;
+});
+listIngredients.append(...elements);
