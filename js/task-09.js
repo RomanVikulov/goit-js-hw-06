938f42959bee63106af9b52267b47bbab569658d
Roman <romanuch06.88@gmail.com>
2022-08-20 19:21:23 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector("body");
+const btnChangeColorRef = document.querySelector(".change-color");
+const bgcRef = document.querySelector(".color");
+
+btnChangeColorRef.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+
+  bodyRef.style.backgroundColor = color;
+  bgcRef.textContent = color;
+});
