6c6149941a580ed52f08f391683f3bdc88453264
Roman <romanuch06.88@gmail.com>
2022-08-20 18:43:53 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all fields!");
+  }
+  const formData = {
+    email: event.currentTarget.email.value,
+    password: event.currentTarget.password.value,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+}
